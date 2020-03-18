a7144292d7937e3d8978c4368acf3d4cf961039c
Museyib Jafarov <museib214@gmail.com>
2020-03-18 07:21:38 -0700

    calculate for one month update

--- task.js
+++ task.js
@@ -5,43 +5,48 @@ let percent = document.getElementById("percent");
 let button = document.getElementById("for-deposit");
 
 function calculateDeposit() {
-    let percentInd = percent.value / 100 / 12;
-    let finalResult = Number(cv.value) + Number(cv.value * percentInd);
-    let refillValue = +refill.value;
-    let plusAmount = 0;
-    for (let i = 0; i < (Number(Math.trunc(days.value / 30)) - 1); i++) {
-        plusAmount = (finalResult +refillValue) * Number(percentInd) + refillValue;
-        finalResult += plusAmount;
+    if (Number(days.value) >= 30) {
+        let percentInd = percent.value / 100 / 12;
+        let finalResult = Number(cv.value) + Number(cv.value * percentInd);
+        let refillValue = +refill.value;
+        let plusAmount = 0;
+        for (let i = 0; i < (Number(Math.trunc(days.value / 30)) - 1); i++) {
+            plusAmount = (finalResult + refillValue) * Number(percentInd) + refillValue;
+            finalResult += plusAmount;
+        }
+        return finalResult;
+    }else{
+        let finalResult = Number(cv.value);
+        return finalResult;
     }
-    return finalResult;
 }
 
-function finalCalculateResult(){
-    document.getElementById("red-tag-four").innerHTML= '';
-    document.getElementById("red-tag-three").innerHTML= '';
-    document.getElementById("red-tag-two").innerHTML= '';
-    document.getElementById("red-tag-one").innerHTML= '';
-if (cv.value >0) {
-    if(refill.value>=0){
-        if(percent.value>0&&percent.value<100){
-            if(days.value>0&&(Math.trunc(days.value)==days.value)){
-                alert(calculateDeposit());
-            }else{
+function finalCalculateResult() {
+    document.getElementById("red-tag-four").innerHTML = '';
+    document.getElementById("red-tag-three").innerHTML = '';
+    document.getElementById("red-tag-two").innerHTML = '';
+    document.getElementById("red-tag-one").innerHTML = '';
+    if (cv.value > 0) {
+        if (refill.value >= 0) {
+            if (percent.value > 0 && percent.value < 100) {
+                if (days.value > 0 && (Math.trunc(days.value) == days.value)) {
+                    alert(calculateDeposit());
+                } else {
+                    console.log('Неверный формат');
+                    document.getElementById("red-tag-four").innerHTML = '&#9888;';
+                }
+            } else {
                 console.log('Неверный формат');
-                document.getElementById("red-tag-four").innerHTML= '&#9888;';
+                document.getElementById("red-tag-three").innerHTML = '&#9888;';
             }
-        }else{
+        } else {
             console.log('Неверный формат');
-            document.getElementById("red-tag-three").innerHTML= '&#9888;';
+            document.getElementById("red-tag-two").innerHTML = '&#9888;';
         }
-    }else{
+    } else {
         console.log('Неверный формат');
-        document.getElementById("red-tag-two").innerHTML= '&#9888;';
+        document.getElementById("red-tag-one").innerHTML = '&#9888;';
     }
-} else {
-    console.log('Неверный формат');
-    document.getElementById("red-tag-one").innerHTML= '&#9888;';
-}
 }
 
 button.addEventListener("click", finalCalculateResult);
